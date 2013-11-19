3aafbd3d78091772893e7c07a75a6da45b633eec
Brad Williams <williamsbrad@me.com>
2013-11-18 16:03:45 -0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,38 @@ describe "Static pages" do
 
     it "should have the content 'Tripbrows'" do
       visit '/static_pages/home'
-      #expect(page).to have_content('Tripbrows')
+      expect(page).to have_content('Tripbrows')
+    end
+    it "should have the title 'Tripbrows'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Tripbrows | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content and title 'Tripbrows'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Tripbrows')
+      expect(page).to have_title("Tripbrows | Help")
+    end
+  end
+  
+  describe "Privacy Page" do
+
+    it "should have the content and title 'Tripbrows'" do
+      visit '/static_pages/privacy'
+      expect(page).to have_content('Tripbrows')
+      expect(page).to have_title("Tripbrows | Privacy")
+    end
+  end
+  
+  describe "Contact page" do
+
+    it "should have the content and title 'Tripbrows'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Tripbrows')
+      expect(page).to have_title("Tripbrows | Contact")
     end
   end
 end
